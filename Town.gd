a3deae0daac1a33a7dbb50e0bc1728365d7ea9a7
Cedor <cedordark@yahoo.fr>
2018-04-26 20:48:56 +0200

    first map

--- Town.gd
+++ Town.gd
@@ -5,11 +5,29 @@ extends Node2D
 # var b = "textvar"
 var mapHeight = 20
 var mapWidth = 40 
+var tileSize = 64
+
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	var tileBar = preload("tiles/TileBar.tscn")
+	var tileWarehouse = preload("tiles/TileWarehouse.tscn")
+	var tilePolice = preload("tiles/TilePolice.tscn")
+	var tileNeutral = preload("tiles/TileNeutral.tscn")
+	var currentTile
+	for i in range(mapHeight):
+		for j in range(mapWidth):
+			var n = randi(8)%8
+			match n:
+				1:
+					currentTile = tileBar.instance();
+				2:
+					currentTile = tileWarehouse.instance();
+				3:
+					currentTile = tilePolice.instance();
+				_:
+					currentTile = tileNeutral.instance();
+			add_child(currentTile)
+			currentTile.position =  Vector2(position.x+j*tileSize,position.y+i*tileSize)
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
